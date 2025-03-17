a69efae6ba7ffa2f77b85e48fa1f7734e160dd86
ddvvs <545515@student.saxion.nl>
2025-03-17 14:45:50 +0100

    animation added
    step delay can be edited through inspector i prefer it around 0.1

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,30 +7,31 @@ public class DungeonGenerator : MonoBehaviour
     [Header("Dungeon Settings")]
     public int dungeonWidth;
     public int dungeonHeight;
-
     public int minRoomSize;
     public int maxSplits;
+    public float stepDelay;
 
     private List<RectInt> rooms = new List<RectInt>();
 
     void Start()
     {
-        GenerateDungeon();
+        StartCoroutine(GenerateDungeon());
     }
 
-    void GenerateDungeon()
+    IEnumerator GenerateDungeon()
     {
         rooms.Clear();
         RectInt mainRoom = new RectInt(0, 0, dungeonWidth, dungeonHeight);
-        SplitRoom(mainRoom, maxSplits);
+        yield return StartCoroutine(SplitRoom(mainRoom, maxSplits));
     }
 
-    void SplitRoom(RectInt room, int depth)
+    IEnumerator SplitRoom(RectInt room, int depth)
     {
         if (depth <= 0 || room.width < minRoomSize * 2 || room.height < minRoomSize * 2)
         {
             rooms.Add(room);
-            return;
+            yield return new WaitForSeconds(stepDelay);
+            yield break;
         }
 
         bool splitHorizontally = Random.value > 0.5f;
@@ -41,16 +43,28 @@ public class DungeonGenerator : MonoBehaviour
             int splitY = Random.Range(room.y + minRoomSize, room.y + room.height - minRoomSize);
             RectInt room1 = new RectInt(room.x, room.y, room.width, splitY - room.y);
             RectInt room2 = new RectInt(room.x, splitY, room.width, room.y + room.height - splitY);
-            SplitRoom(room1, depth - 1);
-            SplitRoom(room2, depth - 1);
+
+            rooms.Add(room1);
+            yield return new WaitForSeconds(stepDelay);
+            yield return StartCoroutine(SplitRoom(room1, depth - 1));
+
+            rooms.Add(room2);
+            yield return new WaitForSeconds(stepDelay);
+            yield return StartCoroutine(SplitRoom(room2, depth - 1));
         }
         else
         {
             int splitX = Random.Range(room.x + minRoomSize, room.x + room.width - minRoomSize);
             RectInt room1 = new RectInt(room.x, room.y, splitX - room.x, room.height);
             RectInt room2 = new RectInt(splitX, room.y, room.x + room.width - splitX, room.height);
-            SplitRoom(room1, depth - 1);
-            SplitRoom(room2, depth - 1);
+
+            rooms.Add(room1);
+            yield return new WaitForSeconds(stepDelay);
+            yield return StartCoroutine(SplitRoom(room1, depth - 1));
+
+            rooms.Add(room2);
+            yield return new WaitForSeconds(stepDelay);
+            yield return StartCoroutine(SplitRoom(room2, depth - 1));
         }
     }
 
